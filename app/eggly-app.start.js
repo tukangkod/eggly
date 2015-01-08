157fa2c2196fdccb25e3993e905d028fbe757839
Fahmi Adam <mohammad@fahmi.my>
2015-01-08 09:42:36 +0800

    editing bookmark and highlight bookmark state

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -55,6 +55,29 @@ angular.module('Eggly', [
 
     $scope.createBookmark = createBookmark;
 
+    $scope.editedBookmark = null;
+
+    function setEditedBookmark(bookmark) {
+        $scope.editedBookmark = angular.copy(bookmark);
+    }
+
+    function updateBookmark(bookmark) {
+        var index = _.findIndex($scope.bookmarks, function(b) {
+            return b.id == bookmark.id;
+        });
+
+        $scope.bookmarks[index] = bookmark;
+        $scope.editedBookmark = null;
+        $scope.isEditing = false;
+    }
+
+    function isSelectedBookmark(bookmarkId) {
+        return $scope.editedBookmark !== null && $scope.editedBookmark.id === bookmarkId;
+    }
+
+    $scope.setEditedBookmark = setEditedBookmark;
+    $scope.updateBookmark = updateBookmark;
+    $scope.isSelectedBookmark = isSelectedBookmark;
 
     //create and edit
     $scope.isCreating = false;
