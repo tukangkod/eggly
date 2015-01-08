002894e7e2ec980103ef45239c1c3c8394a7127b
Fahmi Adam <mohammad@fahmi.my>
2015-01-08 09:56:16 +0800

    delete function

--- app/eggly-app.start.js
+++ app/eggly-app.start.js
@@ -79,6 +79,14 @@ angular.module('Eggly', [
     $scope.updateBookmark = updateBookmark;
     $scope.isSelectedBookmark = isSelectedBookmark;
 
+    function deleteBookmark(bookmark) {
+        _.remove($scope.bookmarks, function(b) {
+            return b.id == bookmark.id;
+        });
+    }
+
+    $scope.deleteBookmark = deleteBookmark;
+
     //create and edit
     $scope.isCreating = false;
     $scope.isEditing = false;
